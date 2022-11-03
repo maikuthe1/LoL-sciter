99e3543b25defc1919a77357b07b9721970a4187
maikuthe1 <linksmallwood@gmail.com>
2022-11-03 19:15:51 +0100

    First content

--- build.sh
+++ build.sh
@@ -4,7 +4,9 @@ cd build
 mkdir linux
 cd linux
 rm * -r
-./extlibs/sciter-js-sdk/bin/linux/packfolder /home/maiku/Documents/Projects/LoL-sciter/resources/ /home/maiku/Documents/Projects/LoL-sciter/src/resources.cpp
+cd ../../extlibs/sciter-js-sdk/bin/linux/
+./packfolder /home/maiku/Documents/Projects/LoL-sciter/resources/ /home/maiku/Documents/Projects/LoL-sciter/src/resources.cpp
+cd ../../../../build/linux
 cmake ../../
 cmake --build .
 cp ../../extlibs/sciter-js-sdk/bin/linux/x64/libsciter.so libsciter.so

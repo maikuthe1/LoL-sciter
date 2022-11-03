6d48b30349d4c7adbc94c7e6a5313c402ec5def0
maikuthe1 <linksmallwood@gmail.com>
2022-11-03 20:05:40 +0100

    Input css

--- build.sh
+++ build.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
-mkdir build
-cd build
-mkdir linux
-cd linux
-rm * -r
-cd ../../extlibs/sciter-js-sdk/bin/linux/
-./packfolder /home/maiku/Documents/Projects/LoL-sciter/resources/ /home/maiku/Documents/Projects/LoL-sciter/src/resources.cpp
-cd ../../../../build/linux
-cmake ../../
-cmake --build .
-cp ../../extlibs/sciter-js-sdk/bin/linux/x64/libsciter.so libsciter.so
+
+while :
+do
+	mkdir build
+    cd build
+    mkdir linux
+    cd linux
+    rm * -r
+    cd ../../extlibs/sciter-js-sdk/bin/linux/
+    ./packfolder /home/maiku/Documents/Projects/LoL-sciter/resources/ /home/maiku/Documents/Projects/LoL-sciter/src/resources.cpp
+    cd ../../../../build/linux
+    cmake ../../
+    cmake --build .
+    cp ../../extlibs/sciter-js-sdk/bin/linux/x64/libsciter.so libsciter.so
+    ./LoL-sciter
+    cd ../../
+done
